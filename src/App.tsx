67a92751363b5b976fa9ead146834c722d75d93c
Sascha Hahne <ntbm@users.noreply.github.com>
2021-10-29 12:04:03 +0200

    Fix Routing

--- src/App.tsx
+++ src/App.tsx
@@ -37,20 +37,32 @@ import Browse from 'screens/manga/Browse';
 
 declare module '@mui/styles/defaultTheme' {
     // eslint-disable-next-line @typescript-eslint/no-empty-interface
-    interface DefaultTheme extends Theme {}
+    interface DefaultTheme extends Theme {
+    }
 }
 
 export default function App() {
     const [title, setTitle] = useState<string>('Tachidesk');
     const [action, setAction] = useState<any>(<div />);
-    const [override, setOverride] = useState<INavbarOverride>({ status: false, value: <div /> });
+    const [override, setOverride] = useState<INavbarOverride>({
+        status: false,
+        value: <div />,
+    });
 
     const [darkTheme, setDarkTheme] = useLocalStorage<boolean>('darkTheme', true);
 
     const navBarContext = {
-        title, setTitle, action, setAction, override, setOverride,
+        title,
+        setTitle,
+        action,
+        setAction,
+        override,
+        setOverride,
     };
-    const darkThemeContext = { darkTheme, setDarkTheme };
+    const darkThemeContext = {
+        darkTheme,
+        setDarkTheme,
+    };
 
     const theme = React.useMemo(
         () => createTheme({
@@ -83,91 +95,95 @@ export default function App() {
         <Router>
             <StyledEngineProvider injectFirst>
                 <ThemeProvider theme={theme}>
-                    <NavbarContext.Provider value={navBarContext}>
-                        <CssBaseline />
-                        <NavBar />
-                        <Container
-                            id="appMainContainer"
-                            maxWidth={false}
-                            disableGutters
-                            style={{
-                                marginTop: theme.spacing(8),
-                                marginLeft: isMobileWidth ? '' : theme.spacing(8),
-                                width: 'auto',
-                                overflow: 'auto',
-                            }}
-                        >
-                            <Switch>
-                                {/* General Routes */}
-                                <Route
-                                    exact
-                                    path="/"
-                                    render={() => (
-                                        <Redirect to="/library" />
-                                    )}
-                                />
+                    <QueryParamProvider ReactRouterRoute={Route}>
+                        <NavbarContext.Provider value={navBarContext}>
+                            <CssBaseline />
+                            <NavBar />
+                            <Container
+                                id="appMainContainer"
+                                maxWidth={false}
+                                disableGutters
+                                style={{
+                                    marginTop: theme.spacing(8),
+                                    marginLeft: isMobileWidth ? '' : theme.spacing(8),
+                                    width: 'auto',
+                                    overflow: 'auto',
+                                }}
+                            >
+                                <Switch>
+                                    {/* General Routes */}
+                                    <Route
+                                        exact
+                                        path="/"
+                                        render={() => (
+                                            <Redirect to="/library" />
+                                        )}
+                                    />
 
-                                <Route path="/settings/about">
-                                    <About />
-                                </Route>
-                                <Route path="/settings/categories">
-                                    <Categories />
-                                </Route>
-                                <Route path="/settings/backup">
-                                    <Backup />
-                                </Route>
-                                <Route path="/settings">
-                                    <DarkTheme.Provider value={darkThemeContext}>
-                                        <Settings />
-                                    </DarkTheme.Provider>
-                                </Route>
+                                    <Route path="/settings/about">
+                                        <About />
+                                    </Route>
+                                    <Route path="/settings/categories">
+                                        <Categories />
+                                    </Route>
+                                    <Route path="/settings/backup">
+                                        <Backup />
+                                    </Route>
+                                    <Route path="/settings">
+                                        <DarkTheme.Provider value={darkThemeContext}>
+                                            <Settings />
+                                        </DarkTheme.Provider>
+                                    </Route>
 
-                                {/* Manga Routes */}
+                                    {/* Manga Routes */}
 
-                                <Route path="/sources/:sourceId/search/">
-                                    <SearchSingle />
-                                </Route>
-                                <Route path="/sources/:sourceId/popular/">
-                                    <SourceMangas popular />
-                                </Route>
-                                <Route path="/sources/:sourceId/latest/">
-                                    <SourceMangas popular={false} />
-                                </Route>
-                                <Route path="/sources/:sourceId/configure/">
-                                    <SourceConfigure />
-                                </Route>
-                                <Route path="/downloads">
-                                    <DownloadQueue />
-                                </Route>
-                                <Route path="/manga/:mangaId/chapter/:chapterNum">
-                                    <></>
-                                </Route>
-                                <Route path="/manga/:id">
-                                    <Manga />
-                                </Route>
-                                <QueryParamProvider ReactRouterRoute={Route}>
+                                    <Route path="/sources/:sourceId/search/">
+                                        <SearchSingle />
+                                    </Route>
+                                    <Route path="/sources/:sourceId/popular/">
+                                        <SourceMangas popular />
+                                    </Route>
+                                    <Route path="/sources/:sourceId/latest/">
+                                        <SourceMangas popular={false} />
+                                    </Route>
+                                    <Route path="/sources/:sourceId/configure/">
+                                        <SourceConfigure />
+                                    </Route>
+                                    <Route path="/downloads">
+                                        <DownloadQueue />
+                                    </Route>
+                                    <Route path="/manga/:mangaId/chapter/:chapterNum">
+                                        <></>
+                                    </Route>
+                                    <Route path="/manga/:id">
+                                        <Manga />
+                                    </Route>
                                     <Route path="/library">
                                         <Library />
                                     </Route>
-                                </QueryParamProvider>
-                                <Route path="/updates">
-                                    <Updates />
-                                </Route>
-                                <Route path="/browse">
-                                    <Browse />
-                                </Route>
+                                    <Route path="/updates">
+                                        <Updates />
+                                    </Route>
+                                    <Route path="/browse">
+                                        <Browse />
+                                    </Route>
+                                </Switch>
+                            </Container>
+                            <Switch>
+                                <Route
+                                    path="/manga/:mangaId/chapter/:chapterIndex"
+                                    // passing a key re-mounts the reader when changing chapters
+                                    render={
+                                        (props: any) => (
+                                            <Reader
+                                                key={props.match.params.chapterIndex}
+                                            />
+                                        )
+                                    }
+                                />
                             </Switch>
-                        </Container>
-                        <Switch>
-                            <Route
-                                path="/manga/:mangaId/chapter/:chapterIndex"
-                                // passing a key re-mounts the reader when changing chapters
-                                render={
-                                    (props:any) => <Reader key={props.match.params.chapterIndex} />
-                                }
-                            />
-                        </Switch>
-                    </NavbarContext.Provider>
+                        </NavbarContext.Provider>
+                    </QueryParamProvider>
                 </ThemeProvider>
             </StyledEngineProvider>
         </Router>
